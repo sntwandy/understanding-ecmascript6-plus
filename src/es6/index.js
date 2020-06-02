6837783772d31d6a23a47573f7b208a80ddf41b9
sntwandy <sntwandy@gmail.com>
2020-06-02 16:13:35 -0400

    Understanding about let, const, spread operator, destructuring and multilines

--- src/es6/index.js
+++ src/es6/index.js
@@ -30,3 +30,64 @@ console.log(epicPhrase);
 
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+// **********************************************
+
+// Multilines before ECMAScript 6
+
+let lorem = 'lorem  ipsum \n' + 'continues' ;
+
+// Multilines after ECMASript 6
+
+let lorem2 = `Other epic phrase
+now is other epic phrase`;
+
+console.log(lorem);
+console.log(lorem2);
+
+// Destructuring before ECMScript 6
+
+let person = {
+    'name': 'Megan',
+    'age': 20,
+    'country': 'DOM',
+}
+
+console.log(person.name, person.age, person.country);
+
+// Destructuring after ECMAScript 6
+
+let { name, age, country } = person;
+
+console.log(name, age, country);
+
+// Spread Operator before ECMAScript 6
+
+let team1 = ['Megan', 'Zeus', 'Darcy'];
+let team2 = ['Atenas', 'Wandy', 'Querty'];
+
+let education1 = ['Gravity', 'Megan', 'Zeus', 'Darcy', 'Atenas', 'Wandy', 'Querty'];
+
+// Spread Operator after ECMAScript 6
+let education2 = ['Gravity', ...team1, ...team2];
+
+console.log( { education2 } );
+
+// Using let with ECMAScript 6
+
+{
+    var globalVar = 'Global var';
+}
+
+{
+    let globalLet = 'Global let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+
+// Using const with ECMAScript 6
+
+const a = 'b';
+a = 'a'; // Output: Error
