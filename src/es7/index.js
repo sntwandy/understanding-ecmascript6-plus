de1db9b1f0c8c0a4dfd918a9406fbb45692710d8
sntwandy <sntwandy@gmail.com>
2020-06-03 17:49:44 -0400

    Understanding ECMAScript 7 (2016), includes, **

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+// includes in ECMAScript 7
+
+let numbers = [24, 29, 17];
+
+if (numbers.includes(17)) {
+    console.log('Its inside');
+} else {
+    console.log('I cant find');
+}
+
+// how to raise to power in ECMAScript 7
+
+let base = 4;
+let exponent = 3;
+
+let result = base ** exponent;
+console.log(result);
